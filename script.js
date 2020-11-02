b9fdd91032992f9d6f8ff4532676c1def04bc6f6
Luis Ramirez <sdaewa22@gmail.com>
2020-11-02 13:16:45 +0000

    form validation

--- script.js
+++ script.js
@@ -6,15 +6,47 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-contaienr');
 
+
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
 }
+
+function validateForm(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('one or both inputs are empty');
+        return false;
+    }
+    if (urlValue.match(regex)) {
+
+    };
+    if (!urlValue.match(regex)) {
+        alert('Enter Valid address');
+        return false;
+    };
+    return true;
+}
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    };
+    if (!validateForm(nameValue, urlValue)) {
+        return false;
+    }
+}
+
 
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => {
     modal.classList.remove('show-modal')
 });
 window.addEventListener('click', (e) => {
-    (e.target === modal) ? modal.classList.remove('show-modal'): false
+    (e.target === modal) ? modal.classList.remove('show-modal'): false;
 });
+bookmarkForm.addEventListener('submit', storeBookmark);
